d9ea66dfd1def68d93ad7136b3624104c7a71046
Jay Kumar <jaykch@outlook.com>
2016-11-11 13:35:58 +0000

    Added tweet functionality

--- index.js
+++ index.js
@@ -3,36 +3,43 @@ $(document).ready(function () {
     getQuote();
 });
 
-var view = (function (){
+var quote = "";
+var author = "";
+
+var view = (function () {
     var currentColor;
 
-    var renderQuote = function(x){
+    var renderQuote = function (x) {
         $('#quote').html("<p><strong>" + x + "</p></strong>");
     };
-    var renderAuthor = function(x){
+    var renderAuthor = function (x) {
         $('#author').html(x);
     };
 
-    var changeColor = function(){
+    var changeColor = function () {
         _randomizeColor();
         $('.change-bg-colour').css("background", currentColor);
         $('#quote').css("color", currentColor);
     };
 
-    var colors = ["#F44336", "#E91E63", "#9C27B0","#673AB7","#3F51B5","#2196F3","#03A9F4","#00BCD4","#009688","#4CAF50",
-        "#8BC34A","#CDDC39","#FFEB3B","#FFC107","#FF9800","#FF5722","#795548","#9E9E9E","#607D8B","#"];
+    var colors = ["#F44336", "#E91E63", "#9C27B0", "#673AB7", "#3F51B5", "#2196F3", "#03A9F4", "#00BCD4", "#009688", "#4CAF50",
+        "#8BC34A", "#FFC107", "#FF9800", "#FF5722", "#795548", "#9E9E9E", "#607D8B", "#"];
 
-    var _randomizeColor = function(){
-        var selector = Math.floor((Math.random() * 20) + 1);
+    var _randomizeColor = function () {
+        var selector = Math.floor((Math.random() * 20) + 1) - 1;
         currentColor = colors[selector];
     };
 
-    return{
+    return {
         renderQuote: renderQuote,
         renderAuthor: renderAuthor,
         changeColor: changeColor
     };
 })();
+
+function tweet() {
+    window.open("https://twitter.com/intent/tweet?text=" + quote + "- " + author, "_blank");
+}
 
 function getQuote() {
     $.ajax({
@@ -43,12 +50,16 @@ function getQuote() {
         },
         url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
         dataType: 'json',
-        success: function(data) {
+        success: function (data) {
             //console.log(data);
             view.renderQuote(data.quote);
             view.renderAuthor(data.author);
             view.changeColor();
+            quote = data.quote;
+            author = data.author;
         },
-        error: function(err) { alert(err); }
+        error: function (err) {
+            alert(err);
+        }
     });
 }
